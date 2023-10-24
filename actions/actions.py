ef2d88f5e47105877a367c75d78fc4b4cb52e2ae
Dat Dang <datd61340@gmail.com>
2023-10-25 00:52:35 +0700

    save user name

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,130 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
-
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from db_sqlite import insert_info, insert_hobby, create_database
-class ActionSaveUserInfo(Action):
-    def name(self):
-        return "action_save_info"
-    def run(self,dispatcher):
-        dispatcher.utter_message(text = "")
+import sqlite3
+from typing import Any, Text, Dict, List
+
+class ActionSaveUserName(Action):
+    def name(self) -> Text:
+        return "action_save_user_name"
+
+    def run(self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        first_name = tracker.get_slot("first_name")
+        
+        insert_info(1,first_name,"","")
+
+        if first_name:
+            dispatcher.utter_message(f"Xin chào, {first_name}!")
+        else:
+            dispatcher.utter_message("Xin chào!")
+
+        return []
+    
+
+class ActionAskCustomerName(Action):
+    def name(self) -> Text:
+        return "action_ask_customer_name"
+
+    def run(self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        user_name = get_user_name("1")
+
+        if user_name:
+            dispatcher.utter_message(
+                response="utter_greet_name",
+                name=user_name
+            )
+        else:
+            dispatcher.utter_message(
+                response="utter_forget_customer_name"
+            )
+
+        return []
+    
+
+class ActionCreateDB(Action):
+    def name(self) -> Text:
+        return "action_create_db"
+
+    def run(self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        # create_database()
+        dispatcher.utter_message("Đã tạo DB")
+
+        return []
+    
+# connect DB
+def create_database():
+    conn = sqlite3.connect("Alio.db")
+    cursor = conn.cursor()
+    sql1 = """
+
+    CREATE TABLE user_info(
+        sender_id VARCHAR,
+        name VARCHAR,
+        age VARCHAR,
+        gender VARCHAR);
+"""
+    sql2 = """
+    
+    CREATE TABLE user_hobby(
+        sender_id VARCHAR,
+        hobby VARCHAR)
+"""
+    cursor.execute(sql1)
+    cursor.execute(sql2)
+    print("create table successfully........")
+
+    # Commit your changes in the database
+    conn.commit()
+
+    #Closing the connection
+    conn.close()
+
+def insert_info(sender_id, name, gender, age):
+    conn = sqlite3.connect("Alio.db")
+    cursor = conn.cursor()
+
+    print("connect to database success!") 
+
+    cursor.execute('''INSERT INTO user_info(sender_id, name, gender, age) VALUES (?, ?, ?, ?)''',(sender_id, name, gender, age))
+    
+    print("insert successfully........")
+
+    conn.commit()
+    conn.close()
+
+def insert_hobby(sender_id, hobby):
+    conn = sqlite3.connect("Alio.db")
+    cursor = conn.cursor()
+
+    print("connect to database success!") 
+
+    cursor.execute('''INSERT INTO user_hobby(sender_id, hobby) VALUES (?, ?)''',(sender_id, hobby))
+
+    print("insert successfully........")
+
+    conn.commit()
+    conn.close()
+
+def get_user_name(sender_id):
+    conn = sqlite3.connect("Alio.db")
+    cursor = conn.cursor()
+    print("sender_id: " , sender_id)
+    print("connect to database success!") 
+
+    cursor.execute('''SELECT * from user_info WHERE sender_id=? LIMIT 1''',(sender_id,))
+    
+    print("select name successfully........")
+    records = cursor.fetchone()
+    print(records)
+    return records
--- actions/db_sqlite.py
+++ actions/db_sqlite.py
@@ -1,51 +1,54 @@
-import sqlite3
+# import sqlite3
 
-def create_database():
-    conn = sqlite3.connect("Alio.db")
-    cursor = conn.cursor()
-    sql = """
+# def create_database():
+#     conn = sqlite3.connect("Alio.db")
+#     cursor = conn.cursor()
+#     sql = """
 
-    CREATE TABLE user_info(
-        sender_id VARCHAR,
-        name VARCHAR,
-        age VARCHAR,
-        gender VARCHAR);
+#     CREATE TABLE user_info(
+#         sender_id VARCHAR,
+#         name VARCHAR,
+#         age VARCHAR,
+#         gender VARCHAR);
     
-    CREATE TABLE user_hobby(
-        sender_id VARCHAR,
-        hobby VARCHAR)
-"""
-    cursor.execute(sql)
-    print("create table successfully........")
+#     CREATE TABLE user_hobby(
+#         sender_id VARCHAR,
+#         hobby VARCHAR)
+# """
+#     cursor.execute(sql)
+#     print("create table successfully........")
 
-    # Commit your changes in the database
-    conn.commit()
+#     # Commit your changes in the database
+#     conn.commit()
 
-    #Closing the connection
-    conn.close()
+#     #Closing the connection
+#     conn.close()
 
-def insert_info(sender_id, name, gender, age):
-    conn = sqlite3.connect("Alio.db")
-    cursor = conn.cursor()
+# def insert_info(sender_id, name, gender, age):
+#     conn = sqlite3.connect("Alio.db")
+#     cursor = conn.cursor()
 
-    print("connect to database success!") 
+#     print("connect to database success!") 
 
-    cursor.execute('''INSERT INTO user_info(sender_id, name, gender, age) VALUES (?, ?, ?, ?)''',(sender_id, name, gender, age))
+#     cursor.execute('''INSERT INTO user_info(sender_id, name, gender, age) VALUES (?, ?, ?, ?)''',(sender_id, name, gender, age))
     
-    print("insert successfully........")
+#     print("insert successfully........")
 
-    conn.commit()
-    conn.close()
+#     conn.commit()
+#     conn.close()
 
-def insert_hobby(sender_id, hobby):
-    conn = sqlite3.connect("Alio.db")
-    cursor = conn.cursor()
+# def insert_hobby(sender_id, hobby):
+#     conn = sqlite3.connect("Alio.db")
+#     cursor = conn.cursor()
 
-    print("connect to database success!") 
+#     print("connect to database success!") 
 
-    cursor.execute('''INSERT INTO user_hobby(sender_id, hobby) VALUES (?, ?)''',(sender_id, hobby))
+#     cursor.execute('''INSERT INTO user_hobby(sender_id, hobby) VALUES (?, ?)''',(sender_id, hobby))
 
-    print("insert successfully........")
+#     print("insert successfully........")
 
-    conn.commit()
-    conn.close()
+#     conn.commit()
+#     conn.close()
+
+# if __name__ == "__main__":
+#     create_database()
